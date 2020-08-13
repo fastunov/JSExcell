375b3376dad943d8ac99a0a5daa037e41521a7e6
Sergey Fatunov <fastunov@gmail.com>
2020-08-14 00:20:52 +0400

    Add basic function for content

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,4 @@
+import {capitalize} from '@core/utils'
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -8,8 +9,25 @@ export class DomListener {
   }
 
   initDomListeners() {
-    console.log(this.listeners)
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error(
+            `Method ${method} is not implemented in ${this.name} component`)
+      }
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
+    })
   }
 
-  removeDomListeners() {}
+  removeDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
